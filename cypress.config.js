5f357b6f65d4f2366b70668e27f353ebbeee7086
ccaiolsa <ccaiolsa@gmail.com>
2026-02-09 12:26:05 -0400

    mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,18 +2,19 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
     reporterOptions: {
-    charts: true,
-    reportPageTitle: 'custom-title',
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
-  }
-},
+      reportDir: 'mochawesome-report',
+      overwrite: false,
+      charts: true,
+      reportPageTitle: 'custom-title',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+  },
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
       // implement node event listeners here
     },
   },
